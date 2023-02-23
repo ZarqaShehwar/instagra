a127fd4a6203f264cb0b69dfc6a5d88dc966593f
ZarqaShehwar <zarqashehwar02@gmail.com>
2023-02-22 23:01:09 -0800

    Initial level

---
+++ lib/Responsive/webscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WebScreen extends StatefulWidget {
+  const WebScreen({super.key});
+
+  @override
+  State<WebScreen> createState() => Screen();
+}
+
+class Screen extends State<WebScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
